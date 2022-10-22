d934acb9c0cfe366c6398ca2a409cfd4c2917bcb
Jonathan Carroll <github@jcarroll.com.au>
2022-10-22 23:37:17 +1030

    working print of position

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use clap::{arg, command, value_parser};
+//use simple_grid::Grid;
+use term_grid::{Grid, GridOptions, Direction, Filling, Cell};
 
 fn main() {
     let matches = command!() // requires `cargo` feature
@@ -20,18 +22,18 @@ fn main() {
     .get_matches();
 
     // You can check the value provided by positional arguments, or option arguments
-    if let Some(fen) = matches.get_one::<String>("fen") {
-        println!("Value for fen: {}", fen);
-    }
+    // if let Some(fen) = matches.get_one::<String>("fen") {
+    //     println!("Value for fen: {}", fen);
+    // }
 
-    if let Some(pretty) = matches.get_one::<bool>("pretty") {
-        println!("Value for pretty: {}", pretty);
-    }
+    // if let Some(pretty) = matches.get_one::<bool>("pretty") {
+    //     println!("Value for pretty: {}", pretty);
+    // }
 
-    if let Some(debug) = matches.get_one::<bool>("debug") {
-        println!("Value for debug: {}", debug);
-    }
-    
+    // if let Some(debug) = matches.get_one::<bool>("debug") {
+    //     println!("Value for debug: {}", debug);
+    // }
+
     // first piece of fen
     let full_fen: String = match matches.get_one::<String>("fen").unwrap().parse() {
         Ok(val) => val,
@@ -45,39 +47,78 @@ fn main() {
     // starting FEN: rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1
     // has 6 parts
     if fenvec.len() != 6 {
-        println!("BAD")
-    } else {
-        println!("GOOD")
-    }
+        println!("BAD FEN!")
+    } //else {
+    //    println!("GOOD")
+    //}
     let fen1: String = fenvec[0].clone();
-    println!("first part of fen: {:?}", fen1);
+    //println!("first part of fen: {:?}", fen1);
 
     // split at separators
     let fenarray: Vec<String> = fen1.split("/").map(str::to_string).collect();
-    println!("Split: {:?}", fenarray);
+    // println!("Split: {:?}", fenarray);
 
-    println!("Test piece translate: p = {}", translate_piece(String::from("p")));
-    println!("Test piece translate: P = {}", translate_piece(String::from("P")));
-    println!("Test piece translate: Q = {}", translate_piece(String::from("Q")));
-    println!("Test piece translate: q = {}", translate_piece(String::from("q")));
+    //println!("Test piece translate: p = {}", translate_piece("p"));
+    //println!("Test piece translate: P = {}", translate_piece("P"));
+    //println!("Test piece translate: Q = {}", translate_piece("Q"));
+    //println!("Test piece translate: q = {}", translate_piece("q"));
+    //println!("Test piece translate: 4 = '{}'", translate_piece("4"));
+
+    let mut fentranslated: Vec<Vec<String>> = Vec::with_capacity(64);
+    for rank in fenarray {
+        let mut rankchars: Vec<String> = Vec::new();
+        for chars in rank.split_inclusive("").filter(|&x| !x.is_empty()) {
+            let t: String = translate_piece(chars).to_string().split("").collect();
+            let i = t.parse::<i32>();
+            let tvec: Vec<String> = match i {
+                Ok(v) => std::iter::repeat(String::from("")).take(v as usize).collect(),
+                Err(_) => vec![t]
+            };
+            for el in tvec {
+                rankchars.push(el);
+            }
+        }
+        fentranslated.push(rankchars);
+    }
+    //println!("Translated: {:?}", fentranslated.concat());
+
+    let mut grid = Grid::new(GridOptions {
+        filling:     Filling::Spaces(1),
+        direction:   Direction::LeftToRight,
+    });
+
+    for s in fentranslated.concat() {
+        grid.add(Cell::from(s.to_string()));
+    }
+
+    println!("{}", grid.fit_into_columns(8));
+
 } 
 
 // sub symbols
-fn translate_piece(x: String) -> String {
-    let newsym: String = match x.as_str() {
-        "p" => String::from("♙"),
-        "n" => String::from("♘"),
-        "b" => String::from("♗"),
-        "r" => String::from("♖"),
-        "q" => String::from("♕"),
-        "k" => String::from("♔"),
-        "P" => String::from("♟"),
-        "N" => String::from("♞"),
-        "B" => String::from("♝"),
-        "R" => String::from("♜"),
-        "Q" => String::from("♛"),
-        "K" => String::from("♚"),
-        _ => String::from("")
+fn translate_piece(x: &str) -> &str {
+    let newsym: &str = match x {
+        "p" => "♟",
+        "n" => "♞",
+        "b" => "♝",
+        "r" => "♜",
+        "q" => "♛",
+        "k" => "♚",
+        "P" => "♙",
+        "N" => "♘",
+        "B" => "♗",
+        "R" => "♖",
+        "Q" => "♕",
+        "K" => "♔",
+        "1" => "1",
+        "2" => "2",
+        "3" => "3",
+        "4" => "4",
+        "5" => "5",
+        "6" => "6",
+        "7" => "7",
+        "8" => "8",     
+        _ => ""
     };
-    newsym.to_string()
+    newsym
 }
